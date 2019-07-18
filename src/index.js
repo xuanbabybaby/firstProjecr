cc90bc621fa79530bb2c485170a7532a3557d4a3
zhangxuan <zhangxuan05@corp.netease.com>
2019-07-18 15:05:24 +0800

    test

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import getRouter from './router/router';
+ReactDom.render(
+    getRouter(),
+    document.getElementById('app')
+)
